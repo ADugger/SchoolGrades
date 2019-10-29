170dbe8d45bf5c3becd2e522e34e5d19948d18f6
AlanDugger <a.duggertnt@gmail.com>
2019-10-29 12:12:43 -0400

    Added additional JUnit tests for SimpleGrade

--- src/test/java/edu/westga/cs/schoolgrades/model/TestSimpleGradeGetValue.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestSimpleGradeGetValue.java
@@ -1,5 +1,6 @@
 package edu.westga.cs.schoolgrades.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -13,9 +14,38 @@ import org.junit.jupiter.api.Test;
  */
 public class TestSimpleGradeGetValue {
 
+	/**
+	 * Test case when attempting to initialize a grade below 0
+	 */
 	@Test
 	void testSimpleGradeBelowZero() {		
-		assertThrows(IllegalArgumentException.class, () -> {SimpleGrade testGrade = new SimpleGrade(-10);});
+		assertThrows(IllegalArgumentException.class, () -> {new SimpleGrade(-10);});
 	}
-
+	
+	/**
+	 * Test case when attempting to retrieve a grade without decimal value
+	 */
+	@Test
+	void testSimpleGradeGetWholeNumberGrade() {		
+		SimpleGrade testGrade = new SimpleGrade(99);
+		assertEquals(99, testGrade.getValue());				
+	}
+	
+	/**
+	 * Test case when attempting to retrieve a grade with a decimal value
+	 */
+	@Test
+	void testSimpleGradeGetDecimalGrade() {		
+		SimpleGrade testGrade = new SimpleGrade(93.7);
+		assertEquals(93.7, testGrade.getValue());
+	}
+	
+	/**
+	 * Test case when attempting to retrieve a grade of 0
+	 */
+	@Test
+	void testSimpleGradeGetGradeOfZero() {		
+		SimpleGrade testGrade = new SimpleGrade(0);
+		assertEquals(0, testGrade.getValue());
+	}
 }
