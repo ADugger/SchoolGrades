f3141adc3a6cfc6bf0d2e75aa99fa52e82ac8f0b
AlanDugger <a.duggertnt@gmail.com>
2019-11-09 14:52:16 -0500

    Redid cellfactory for quiz listview to use StringConverter
    Added controller to fxml file

--- src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
+++ src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
@@ -1,21 +1,26 @@
 package edu.westga.cs.schoolgrades.controllers;
 
+import java.util.List;
+
 import edu.westga.cs.schoolgrades.model.SimpleGrade;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.TextFieldListCell;
 import javafx.util.Callback;
+import javafx.util.StringConverter;
 
 /**
  * Class to control the FXML GUI for the School Grades project and inject FXML references
@@ -28,10 +33,10 @@ public class SchoolGradesController {
 	private ObservableList<SimpleGrade> quizGrades;
 	private ObservableList<SimpleGrade> homeworkGrades;
 	private ObservableList<SimpleGrade> examGrades;
-	private IntegerProperty quizSubtotal = new SimpleIntegerProperty(0);
-	private IntegerProperty homeworkSubtotal = new SimpleIntegerProperty(0);
-	private IntegerProperty examSubtotal = new SimpleIntegerProperty(0);
-	private IntegerProperty finalTotal = new SimpleIntegerProperty(0);
+	private DoubleProperty quizSubtotal;
+	private DoubleProperty homeworkSubtotal;
+	private DoubleProperty examSubtotal;
+	private DoubleProperty finalTotal;
 	
 	@FXML
 	private ListView<SimpleGrade> lvQuiz;
@@ -60,12 +65,16 @@ public class SchoolGradesController {
 	private MenuItem mitemNewExam;
 	
 	/**
-	 * Constructor method to allow controller instantiation and initialization of observable lists
+	 * Constructor method to allow controller instantiation and initialization variables
 	 */
 	public SchoolGradesController() {	
 		this.quizGrades = FXCollections.observableArrayList();			
 		this.homeworkGrades = FXCollections.observableArrayList();
 		this.examGrades = FXCollections.observableArrayList();
+		this.quizSubtotal = new SimpleDoubleProperty(0);
+		this.homeworkSubtotal = new SimpleDoubleProperty(0);
+		this.examSubtotal = new SimpleDoubleProperty(0);
+		this.finalTotal = new SimpleDoubleProperty(0);
 	}
 	
 	/**
@@ -77,14 +86,13 @@ public class SchoolGradesController {
 		this.txtHomeworkSubtotal.textProperty().bind(this.homeworkSubtotal.asString());
 		this.txtExamSubtotal.textProperty().bind(this.examSubtotal.asString());
 		this.txtFinalGrade.textProperty().bind(this.finalTotal.asString());
-		this.listviewQuizSetup();
-		this.mitemNewQuiz.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				SchoolGradesController.this.quizGrades.add(new SimpleGrade(0.0));				
-			}
-		});
+		this.listviewQuizSetup();		
 	}	
+	
+	@FXML
+	private void handleNewQuizClick(ActionEvent event) {
+		this.quizGrades.add(new SimpleGrade(0.0));
+	}
 	
 	private void listviewQuizSetup() {
 		this.lvQuiz.setItems(this.quizGrades);
@@ -92,15 +100,26 @@ public class SchoolGradesController {
 		this.lvQuiz.setCellFactory(new Callback<ListView<SimpleGrade>, ListCell<SimpleGrade>>() {
 			@Override
 			public ListCell<SimpleGrade> call(ListView<SimpleGrade> theCall) {
-				 TextFieldListCell<SimpleGrade> oneCell = new TextFieldListCell<SimpleGrade>() {
-					 @Override
-					 public void updateItem(SimpleGrade theGrade, boolean empty) {
-						super.updateItem(theGrade, empty);
-						if (!empty) {
-							setText(String.valueOf(theGrade.getValue()));
-						}
-				 	}
-				 };
+				 TextFieldListCell<SimpleGrade> oneCell = new TextFieldListCell<SimpleGrade>();
+				 oneCell.setConverter(new StringConverter<SimpleGrade>() {
+					 public String toString(SimpleGrade theGrade) {
+						 return String.format("%.2f", theGrade.getValue());
+					 }
+					 
+					 public SimpleGrade fromString(String theCellString) {
+						 try {
+							 double convertedValue = Double.parseDouble(theCellString);
+							 SimpleGrade convertedGrade = new SimpleGrade(convertedValue);
+							 return convertedGrade;
+						 } catch (NumberFormatException nfe) {
+							Alert errorAlert = new Alert(AlertType.ERROR);
+							errorAlert.setTitle("Not A Number");
+							errorAlert.setContentText("There was an error the number you entered for the grade." +nfe.getMessage());
+							errorAlert.showAndWait();
+						 }
+						return null;
+					 }
+				 });
 				 oneCell.setEditable(true);
 				 return oneCell;
 			}			
