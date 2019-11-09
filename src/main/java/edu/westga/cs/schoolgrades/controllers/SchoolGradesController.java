c161f1dd12cecbd288e59865c141a59c14540834
AlanDugger <a.duggertnt@gmail.com>
2019-11-09 11:44:25 -0500

    Created GradesGUI layout via SceneBuilder
    Created SchoolGradesController stub

---
+++ src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
@@ -0,0 +1,11 @@
+package edu.westga.cs.schoolgrades.controllers;
+
+/**
+ * Class to control the FXML GUI for the School Grades project and inject FXML references
+ * 
+ * @author Alan Dugger
+ * @version 11/09/2019
+ */
+public class SchoolGradesController {
+
+}
