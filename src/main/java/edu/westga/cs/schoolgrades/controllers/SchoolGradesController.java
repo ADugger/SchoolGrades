30c866019526f901295f8b3c908e61f3e9d01107
AlanDugger <a.duggertnt@gmail.com>
2019-11-09 13:00:16 -0500

    Working on setting up CellFactory for each observable lists of grades

--- src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
+++ src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
@@ -1,11 +1,20 @@
 package edu.westga.cs.schoolgrades.controllers;
 
 import edu.westga.cs.schoolgrades.model.Grade;
+import edu.westga.cs.schoolgrades.model.SimpleGrade;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 /**
  * Class to control the FXML GUI for the School Grades project and inject FXML references
@@ -15,12 +24,20 @@ import javafx.scene.control.TextField;
  */ 
 public class SchoolGradesController {
 	
+	private ObservableList<SimpleGrade> quizGrades;
+	private ObservableList<SimpleGrade> homeworkGrades;
+	private ObservableList<SimpleGrade> examGrades;
+	private IntegerProperty quizSubtotal = new SimpleIntegerProperty(0);
+	private IntegerProperty homeworkSubtotal = new SimpleIntegerProperty(0);
+	private IntegerProperty examSubtotal = new SimpleIntegerProperty(0);
+	private IntegerProperty finalTotal = new SimpleIntegerProperty(0);
+	
 	@FXML
-	private ListView<Grade> lvQuiz;
+	private ListView<SimpleGrade> lvQuiz;
 	@FXML
-	private ListView<Grade> lvHomework;
+	private ListView<SimpleGrade> lvHomework;
 	@FXML
-	private ListView<Grade> lvExam;
+	private ListView<SimpleGrade> lvExam;
 	
 	@FXML
 	private TextField txtQuizSubtotal;
@@ -40,4 +57,36 @@ public class SchoolGradesController {
 	private MenuItem mitemNewHomework;
 	@FXML
 	private MenuItem mitemNewExam;
+	
+	/**
+	 * Constructor method to allow controller instantiation and initialization of observable lists
+	 */
+	public SchoolGradesController() {	
+		this.quizGrades = FXCollections.observableArrayList();		
+		this.homeworkGrades = FXCollections.observableArrayList();
+		this.examGrades = FXCollections.observableArrayList();
+	}
+	
+	/**
+	 * FXML initialization of GUI components
+	 */
+	@FXML
+	private void initialize() {
+		this.txtQuizSubtotal.textProperty().bind(this.quizSubtotal.asString());
+		this.txtHomeworkSubtotal.textProperty().bind(this.homeworkSubtotal.asString());
+		this.txtExamSubtotal.textProperty().bind(this.examSubtotal.asString());
+		this.txtFinalGrade.textProperty().bind(this.finalTotal.asString());
+		this.listviewQuizSetup();
+		this.mitemNewQuiz.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				SchoolGradesController.this.quizGrades.add(new SimpleGrade(0.0));				
+			}
+		});
+	}	
+	
+	private void listviewQuizSetup() {
+		this.lvQuiz.setItems(this.quizGrades);
+		this.lvQuiz.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);		
+	}
 }
