d5b286d4830c5b29a4040ef70131e1f4ede894cb
AlanDugger <a.duggertnt@gmail.com>
2019-11-25 18:45:05 -0500

    Added mocked objects to TestCompositeGradeConstructor and Add tests

--- src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeConstructor.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeConstructor.java
@@ -1,11 +1,19 @@
 package edu.westga.cs.schoolgrades.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestCompositeGradeConstructor {
-
+	private SumOfGradesStrategy mockStrategy;
 	
+	@BeforeEach
+	public void setup() {
+		mockStrategy = mock(SumOfGradesStrategy.class);		
+	}
 	
 	@Test
 	public void shouldNotAllowNullStrategy() {
@@ -16,7 +24,7 @@ public class TestCompositeGradeConstructor {
 	
 	@Test
 	public void shouldHaveNoGradesWhenCreated() {
-		CompositeGrade grade = new CompositeGrade(new SumOfGradesStrategy());
+		CompositeGrade grade = new CompositeGrade(mockStrategy);
 		assertTrue(grade.getGrades().isEmpty());
 	}
 
